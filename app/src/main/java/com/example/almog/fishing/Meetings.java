32893edefb04651a26bd072c01368b563cab83c4
Almog <mogdog17@gmail.com>
2016-05-11 17:15:16 +0300

    ok? mm

--- app/src/main/java/com/example/almog/fishing/Meetings.java
+++ app/src/main/java/com/example/almog/fishing/Meetings.java
@@ -24,7 +24,7 @@ public class Meetings extends AppCompatActivity
 
     ListView listView ;
     final Context context = this;
-
+    static int i=0;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -42,14 +42,12 @@ public class Meetings extends AppCompatActivity
             {//id: The row id of the item that was clicked.
 //position-int: The position of the view in the adapter.
 //parent-: The AdapterView where the click happened.
-
                 final ListView MyListView=(ListView)findViewById(R.id.list);
                 final Dialog dialog = new Dialog(context);
                 TextView[] textViewArray = new TextView[MyListView.getLastVisiblePosition()];
-                TextView  text = (TextView) dialog.findViewById(R.id.textViewDetiled);// define textview of detiled window
-
+                //TextView  text = (TextView) dialog.findViewById(R.id.textViewDetiled);// define textview of detiled window
                 dialog.setContentView(R.layout.custom);// connect the detiled layout with the dialog
-                text.setText(Dataset.DetilesDB.get(    );//any click!!  set text in the textview of the detiled window
+                textViewArray[i].setText(Dataset.DetilesDB.get(    );//any click!!  set text in the textview of the detiled window
                 dialog.show();
                //int itemPosition = position;
               // String itemValue = (String)listView.getItemAtPosition(M.getLastVisiblePosition());
